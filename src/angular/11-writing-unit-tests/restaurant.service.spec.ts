bbbd541e72e3bb241f59d3991986932f8ed65126
tehfedaykin <jennifer.wadella@gmail.com>
2019-02-06 15:25:08 -0500

    adding restaurant service unit test

--- src/angular/11-writing-unit-tests/restaurant.service.spec.ts
+++ src/angular/11-writing-unit-tests/restaurant.service.spec.ts
@@ -1,16 +1,148 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RestaurantService } from './restaurant.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('RestaurantService', () => {
+  let httpMock : HttpTestingController;
+  let restaurantService: RestaurantService;
+
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [ {provide: HttpClient, useClass: HttpClientTestingModule} ]
+    imports: [HttpClientTestingModule],
+    providers: [ 
+      RestaurantService
+    ]
   }));
 
+  beforeEach(() => {
+    httpMock = TestBed.get(HttpTestingController);
+    restaurantService = TestBed.get(RestaurantService);
+  })
+
   it('should be created', () => {
-    const service: RestaurantService = TestBed.get(RestaurantService);
-    expect(service).toBeTruthy();
+    expect(restaurantService).toBeTruthy();
   });
+
+  it('should make a get request to states', () => {
+    const mockStates = {
+      data: [
+        {name: 'Missouri', short: 'MO'}
+      ]
+    };
+
+    restaurantService.getStates().subscribe((states) => {
+      expect(states).toEqual(mockStates);
+    });
+    
+    let url = '/api/states';
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockStates);
+
+    httpMock.verify();
+  });
+
+  it('should make a get request to cities', () => {
+    const mockCities = {
+      data: [
+        {name: 'Kansas City', state: 'MO'}
+      ]
+    };
+
+    restaurantService.getCities('MO').subscribe((cities) => {
+      expect(cities).toEqual(mockCities);
+    });
+    
+    let url = '/api/cities?filter[state]=MO';
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockCities);
+
+    httpMock.verify();
+  })
+
+  it('should make a get request to restaurants', () => {
+    const mockRestaurants = {
+      data: [
+        {"name":"Brunch Place",
+        "slug":"brunch-place",
+        "images":{
+          "thumbnail":"node_modules/place-my-order-assets/images/4-thumbnail.jpg",
+          "owner":"node_modules/place-my-order-assets/images/2-owner.jpg",
+          "banner":"node_modules/place-my-order-assets/images/2-banner.jpg"},
+          "menu":{
+            "lunch":[
+              {"name":"Ricotta Gnocchi","price":15.99},
+              {"name":"Garlic Fries","price":15.99},
+              {"name":"Charred Octopus","price":25.99}
+            ],
+            "dinner":[
+              {"name":"Steamed Mussels","price":21.99},
+              {"name":"Roasted Salmon","price":23.99},
+              {"name":"Crab Pancakes with Sorrel Syrup","price":35.99}
+            ]
+          },
+          "address":{
+            "street":"2451 W Washburne Ave",
+            "city":"Ann Arbor","state":"MI","zip":"53295"},
+            "_id":"xugqxQIX5rPJTLBv"
+          }
+      ]
+    };
+      
+    restaurantService.getRestaurants('MO', 'Kansas City').subscribe((restaurants) => {
+      expect(restaurants).toEqual(mockRestaurants);
+    });
+    
+    let url = 'api/restaurants?filter%5Baddress.state%5D=MO&filter%5Baddress.city%5D=Kansas+City';
+    const req = httpMock.expectOne(url);
+    console.log('request', req);
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockRestaurants);
+
+    httpMock.verify();
+  });
+
+  it('should make a get request to restaurant', () => {
+    const mockRestaurant = {
+      "name":"Brunch Place",
+      "slug":"brunch-place",
+      "images":{
+        "thumbnail":"node_modules/place-my-order-assets/images/4-thumbnail.jpg",
+        "owner":"node_modules/place-my-order-assets/images/2-owner.jpg",
+        "banner":"node_modules/place-my-order-assets/images/2-banner.jpg"},
+        "menu":{
+          "lunch":[
+            {"name":"Ricotta Gnocchi","price":15.99},
+            {"name":"Garlic Fries","price":15.99},
+            {"name":"Charred Octopus","price":25.99}
+          ],
+          "dinner":[
+            {"name":"Steamed Mussels","price":21.99},
+            {"name":"Roasted Salmon","price":23.99},
+            {"name":"Crab Pancakes with Sorrel Syrup","price":35.99}
+          ]
+        },
+        "address":{
+          "street":"2451 W Washburne Ave",
+          "city":"Ann Arbor","state":"MI","zip":"53295"},
+          "_id":"xugqxQIX5rPJTLBv"
+        };
+
+    restaurantService.getRestaurant('potato-palace').subscribe((restaurant) => {
+      expect(restaurant).toEqual(mockRestaurant);
+    });
+    
+    let url = '/api/restaurants/potato-palace?';
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockRestaurant);
+
+    httpMock.verify();
+  })
+
 });
