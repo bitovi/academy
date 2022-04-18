8999f36d46a4e41795abc6a88727351d0a809422
fabioemoutinho <fabioemoutinho@gmail.com>
2022-04-18 19:41:18 -0300

    Page Updates: nested-routes - update files to Angular 13

--- src/angular/13-nested-routes/detail.component.spec.ts
+++ src/angular/13-nested-routes/detail.component.spec.ts
@@ -1,60 +1,59 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs'; 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
-import { DetailComponent } from './detail.component';
-import { RestaurantService } from '../restaurant.service';
+import { of } from 'rxjs';
 import { ImageUrlPipe } from 'src/app/image-url.pipe';
+import { RestaurantService } from '../restaurant.service';
+import { DetailComponent } from './detail.component';
 
 class MockRestaurantService {
-  getRestaurant(slug:string) {
+  getRestaurant(slug: string) {
     return of({
-        "name": "Poutine Palace",
-        "slug": "poutine-palace",
-        "images": {
-          "thumbnail": "node_modules/place-my-order-assets/images/4-thumbnail.jpg",
-          "owner": "node_modules/place-my-order-assets/images/3-owner.jpg",
-          "banner": "node_modules/place-my-order-assets/images/2-banner.jpg"
-        },
-        "menu": {
-          "lunch": [
-            {
-              "name": "Crab Pancakes with Sorrel Syrup",
-              "price": 35.99
-            },
-            {
-              "name": "Steamed Mussels",
-              "price": 21.99
-            },
-            {
-              "name": "Spinach Fennel Watercress Ravioli",
-              "price": 35.99
-            }
-          ],
-          "dinner": [
-            {
-              "name": "Gunthorp Chicken",
-              "price": 21.99
-            },
-            {
-              "name": "Herring in Lavender Dill Reduction",
-              "price": 45.99
-            },
-            {
-              "name": "Chicken with Tomato Carrot Chutney Sauce",
-              "price": 45.99
-            }
-          ]
-        },
-        "address": {
-          "street": "230 W Kinzie Street",
-          "city": "Green Bay",
-          "state": "WI",
-          "zip": "53205"
-        },
-        "_id": "3ZOZyTY1LH26LnVw"
-      })
+      name: 'Poutine Palace',
+      slug: 'poutine-palace',
+      images: {
+        thumbnail: 'node_modules/place-my-order-assets/images/4-thumbnail.jpg',
+        owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
+        banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
+      },
+      menu: {
+        lunch: [
+          {
+            name: 'Crab Pancakes with Sorrel Syrup',
+            price: 35.99,
+          },
+          {
+            name: 'Steamed Mussels',
+            price: 21.99,
+          },
+          {
+            name: 'Spinach Fennel Watercress Ravioli',
+            price: 35.99,
+          },
+        ],
+        dinner: [
+          {
+            name: 'Gunthorp Chicken',
+            price: 21.99,
+          },
+          {
+            name: 'Herring in Lavender Dill Reduction',
+            price: 45.99,
+          },
+          {
+            name: 'Chicken with Tomato Carrot Chutney Sauce',
+            price: 45.99,
+          },
+        ],
+      },
+      address: {
+        street: '230 W Kinzie Street',
+        city: 'Green Bay',
+        state: 'WI',
+        zip: '53205',
+      },
+      _id: '3ZOZyTY1LH26LnVw',
+    });
   }
 }
 
@@ -62,30 +61,32 @@ const MockActivatedRoute = {
   snapshot: {
     paramMap: {
       get() {
-        return 'poutine-palace'
-      }
-    }
-  }
-}
+        return 'poutine-palace';
+      },
+    },
+  },
+};
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DetailComponent, ImageUrlPipe ],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DetailComponent, ImageUrlPipe],
       imports: [RouterTestingModule],
-      providers: [{
-        provide: RestaurantService,
-        useClass: MockRestaurantService
-      }, {
-        provide: ActivatedRoute,
-        useValue: MockActivatedRoute
-      }]
-    })
-    .compileComponents();
-  }));
+      providers: [
+        {
+          provide: RestaurantService,
+          useClass: MockRestaurantService,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: MockActivatedRoute,
+        },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DetailComponent);
@@ -103,54 +104,52 @@ describe('DetailComponent', () => {
   });
 
   it('should get a restaurant based on route slug', () => {
-    let mockRestaurant = {
-      "name": "Poutine Palace",
-      "slug": "poutine-palace",
-      "images": {
-        "thumbnail": "node_modules/place-my-order-assets/images/4-thumbnail.jpg",
-        "owner": "node_modules/place-my-order-assets/images/3-owner.jpg",
-        "banner": "node_modules/place-my-order-assets/images/2-banner.jpg"
+    const mockRestaurant = {
+      name: 'Poutine Palace',
+      slug: 'poutine-palace',
+      images: {
+        thumbnail: 'node_modules/place-my-order-assets/images/4-thumbnail.jpg',
+        owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
+        banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
       },
-      "menu": {
-        "lunch": [
+      menu: {
+        lunch: [
           {
-            "name": "Crab Pancakes with Sorrel Syrup",
-            "price": 35.99
+            name: 'Crab Pancakes with Sorrel Syrup',
+            price: 35.99,
           },
           {
-            "name": "Steamed Mussels",
-            "price": 21.99
+            name: 'Steamed Mussels',
+            price: 21.99,
           },
           {
-            "name": "Spinach Fennel Watercress Ravioli",
-            "price": 35.99
-          }
+            name: 'Spinach Fennel Watercress Ravioli',
+            price: 35.99,
+          },
         ],
-        "dinner": [
+        dinner: [
           {
-            "name": "Gunthorp Chicken",
-            "price": 21.99
+            name: 'Gunthorp Chicken',
+            price: 21.99,
           },
           {
-            "name": "Herring in Lavender Dill Reduction",
-            "price": 45.99
+            name: 'Herring in Lavender Dill Reduction',
+            price: 45.99,
           },
           {
-            "name": "Chicken with Tomato Carrot Chutney Sauce",
-            "price": 45.99
-          }
-        ]
+            name: 'Chicken with Tomato Carrot Chutney Sauce',
+            price: 45.99,
+          },
+        ],
       },
-      "address": {
-        "street": "230 W Kinzie Street",
-        "city": "Green Bay",
-        "state": "WI",
-        "zip": "53205"
+      address: {
+        street: '230 W Kinzie Street',
+        city: 'Green Bay',
+        state: 'WI',
+        zip: '53205',
       },
-      "_id": "3ZOZyTY1LH26LnVw"
-    }
-    const fixture = TestBed.createComponent(DetailComponent);
-    fixture.detectChanges();
-    expect(fixture.componentInstance.restaurant).toEqual(mockRestaurant)
+      _id: '3ZOZyTY1LH26LnVw',
+    };
+    expect(fixture.componentInstance.restaurant).toEqual(mockRestaurant);
   });
 });
