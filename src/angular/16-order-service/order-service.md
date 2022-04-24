432b3406f8d39312cec1a444993c730c8afdfeca
fabioemoutinho <fabioemoutinho@gmail.com>
2022-04-23 22:31:51 -0300

    Page Updates: order-service - update files to angular 13

--- src/angular/16-order-service/order-service.md
+++ src/angular/16-order-service/order-service.md
@@ -16,15 +16,29 @@ In this part, we will:
 
 ## Problem 1: Create Order Service and Export Items and Order interfaces
 
-We need to create a new service to handle creating and updating orders. We'll need two interfaces - one to describe the order, one to describe items in the order.
+We need to create a new service to handle creating and updating orders. We'll need three interfaces - one to describe the order form data, one to describe the order, and one to describe items in the order.
 
 ## P1: Technical Requirements
 
-Create a new service `order` in the order directory, and write and export `Order` and `Item` interfaces representing these objects in the new service:
+Create a new service `order` in the order directory, and write and export `OrderForm`, `Order` and `Item` interfaces representing these objects in the new service:
 
 ```typescript
-let myorder = {
+const orderForm = {
+  restaurant: '12345',
+  name: 'Jennifer',
+  address: '123 Main st',
+  phone: '867-5309',
+  items: [
+    {
+      name: 'tacos',
+      price: 6.99,
+    },
+  ],
+};
+
+const order = {
   _id: 'a123123bdd',
+  restaurant: '12345',
   name: 'Jennifer',
   address: '123 Main st',
   phone: '867-5309',
@@ -32,10 +46,10 @@ let myorder = {
   items: [
     {
       name: 'tacos',
-      price: 6.99
-    }
-  ]
-}
+      price: 6.99,
+    },
+  ],
+};
 ```
 
 ## P1: What you Need to Know
@@ -49,10 +63,10 @@ let myorder = {
 
 ## P1: Solution
 
-✏️ Update __src/app/order/order.service.ts__
+✏️ Update **src/app/order/order.service.ts**
 
 @sourceref ./order.service-interfaces.ts
-@highlight 3-6, 8-15
+@highlight 3-6, 8-14, 16-24
 
 ## Problem 2: Finish the Order Service
 
@@ -60,7 +74,7 @@ With our order service we'll want to be able to create new orders, updating exis
 
 ## P2: Setup
 
-✏️ Paste the following into __src/app/order/order.service.spec.ts__:
+✏️ Paste the following into **src/app/order/order.service.spec.ts**:
 
 @sourceref ./2-order.service.spec.ts
 
@@ -79,13 +93,12 @@ ng test
 - You will need to look at the test code to determine
   the method signatures on `OrderService`.
 - You will need to make sure `HttpClient` is imported and
-  added as a property in the  `OrderService` constructor.
+  added as a property in the `OrderService` constructor.
 
 ## P2: Solution
 
-✏️ Update __src/app/order/order.service.ts__
+✏️ Update **src/app/order/order.service.ts**
 @diff ./order.service-interfaces.ts ./order-2.service.ts only
-
 
 ## Problem 3: Use the OrderService in the OrderComponent to Create an Order
 
@@ -101,24 +114,23 @@ For this problem, we will:
 How we will solve this:
 
 1. We will import the order service, and save it as `orderService` in the
-  `OrderComponent`'s `constructor`.
+   `OrderComponent`'s `constructor`.
 2. Call `orderService`'s `createOrder` with the `orderForm`'s values.
 3. While the order is being created `orderProcessing` should be `true`.
 4. Once complete, `orderComplete` should be set to `true`
-  and set back to `false` when `.startNewOrder()` is called.
+   and set back to `false` when `startNewOrder()` is called.
 5. We will save the completed order in `completedOrder`.
 
 ## P3: Setup
 
 Before starting:
 
-1\. ✏️ Update __src/app/order/order.component.html__ to show the completed order:
+1\. ✏️ Update **src/app/order/order.component.html** to show the completed order:
 
 @diff ../15-directive/order.component.directive.html ./order.component.html only
 
-
-2\. ✏️ Update __src/app/order/order.component.ts__ to have a `onSubmit` method and
-    a `startNewOrder` that will start a new order.
+2\. ✏️ Update **src/app/order/order.component.ts** to have a `onSubmit` method and
+a `startNewOrder` that will start a new order.
 
 @diff ../14-building-order-form/child-component/order-2.component.ts ./order.component.ts only
 
@@ -126,10 +138,9 @@ Before starting:
 
 If you've implemented everything correctly, you should now be able to create an order from the UI and see a record of your completed order once it's created.
 
-✏️ Update the menu-items spec file  __src/app/order/order.component.spec.ts__ to be:
+✏️ Update the menu-items spec file **src/app/order/order.component.spec.ts** to be:
 
 @diff ../14-building-order-form/order.component.spec-final.ts ./order.component.spec.ts only
-
 
 > If you've implemented the solution correctly, when you run `npm run test` all tests will pass!
 
@@ -141,5 +152,5 @@ If you've implemented everything correctly, you should now be able to create an 
 
 ## P3: Solution
 
-✏️ Update __src/app/order/order.component.ts__
+✏️ Update **src/app/order/order.component.ts**
 @diff ./order.component.ts ./order.component-solution.ts only
--- src/angular/16-order-service/order.component.spec.ts
+++ src/angular/16-order-service/order.component.spec.ts
@@ -1,77 +1,79 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs'; 
-
-import { OrderComponent } from './order.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RestaurantService } from '../restaurant/restaurant.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { RestaurantService } from '../restaurant/restaurant.service';
 import { MenuItemsComponent } from './menu-items/menu-items.component';
-import { OrderService } from './order.service';
+import { OrderComponent } from './order.component';
+import { OrderForm, OrderService } from './order.service';
 
 class MockRestaurantService {
-  getRestaurant(slug:string) {
+  getRestaurant(slug: string) {
     return of({
-        "name": "Poutine Palace",
-        "slug": "poutine-palace",
-        "images": {
-          "thumbnail": "node_modules/place-my-order-assets/images/4-thumbnail.jpg",
-          "owner": "node_modules/place-my-order-assets/images/3-owner.jpg",
-          "banner": "node_modules/place-my-order-assets/images/2-banner.jpg"
-        },
-        "menu": {
-          "lunch": [
-            {
-              "name": "Crab Pancakes with Sorrel Syrup",
-              "price": 35.99
-            },
-            {
-              "name": "Steamed Mussels",
-              "price": 21.99
-            },
-            {
-              "name": "Spinach Fennel Watercress Ravioli",
-              "price": 35.99
-            }
-          ],
-          "dinner": [
-            {
-              "name": "Gunthorp Chicken",
-              "price": 21.99
-            },
-            {
-              "name": "Herring in Lavender Dill Reduction",
-              "price": 45.99
-            },
-            {
-              "name": "Chicken with Tomato Carrot Chutney Sauce",
-              "price": 45.99
-            }
-          ]
-        },
-        "address": {
-          "street": "230 W Kinzie Street",
-          "city": "Green Bay",
-          "state": "WI",
-          "zip": "53205"
-        },
-        "_id": "3ZOZyTY1LH26LnVw"
-      })
+      name: 'Poutine Palace',
+      slug: 'poutine-palace',
+      images: {
+        thumbnail: 'node_modules/place-my-order-assets/images/4-thumbnail.jpg',
+        owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
+        banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
+      },
+      menu: {
+        lunch: [
+          {
+            name: 'Crab Pancakes with Sorrel Syrup',
+            price: 35.99,
+          },
+          {
+            name: 'Steamed Mussels',
+            price: 21.99,
+          },
+          {
+            name: 'Spinach Fennel Watercress Ravioli',
+            price: 35.99,
+          },
+        ],
+        dinner: [
+          {
+            name: 'Gunthorp Chicken',
+            price: 21.99,
+          },
+          {
+            name: 'Herring in Lavender Dill Reduction',
+            price: 45.99,
+          },
+          {
+            name: 'Chicken with Tomato Carrot Chutney Sauce',
+            price: 45.99,
+          },
+        ],
+      },
+      address: {
+        street: '230 W Kinzie Street',
+        city: 'Green Bay',
+        state: 'WI',
+        zip: '53205',
+      },
+      _id: '3ZOZyTY1LH26LnVw',
+    });
   }
 }
 
 class MockOrderService {
-  createOrder(order) {
+  createOrder(order: OrderForm) {
     return of({
-      "address": null,
-      "items": [{"name": "Onion fries", "price": 15.99}, {"name": "Roasted Salmon", "price": 23.99}],
-      "name": "Jennifer Hungry",
-      "phone": null,
-      "restaurant": "uPkA2jiZi24tCvXh",
-      "status": "preparing",
-      "_id": "0awcHyo3iD6CpvhX",
-    })
+      address: null,
+      items: [
+        { name: 'Onion fries', price: 15.99 },
+        { name: 'Roasted Salmon', price: 23.99 },
+      ],
+      name: 'Jennifer Hungry',
+      phone: null,
+      restaurant: 'uPkA2jiZi24tCvXh',
+      status: 'preparing',
+      _id: '0awcHyo3iD6CpvhX',
+    });
   }
 }
 
@@ -79,38 +81,39 @@ const MockActivatedRoute = {
   snapshot: {
     paramMap: {
       get() {
-        return 'poutine-palace'
-      }
-    }
-  }
-}
+        return 'poutine-palace';
+      },
+    },
+  },
+};
 
 describe('OrderComponent', () => {
   let component: OrderComponent;
   let fixture: ComponentFixture<OrderComponent>;
-  let injectedOrderService;
+  let orderService: OrderService;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrderComponent, MenuItemsComponent ], 
-      imports: [  ReactiveFormsModule, RouterTestingModule ],
-      providers: [{
-        provide: RestaurantService,
-        useClass: MockRestaurantService
-      }, {
-        provide: ActivatedRoute,
-        useValue: MockActivatedRoute
-      },
-      {
-        provide: OrderService,
-        useClass: MockOrderService
-      }],
-      schemas: [ NO_ERRORS_SCHEMA ]
-    })
-    .compileComponents();
-    injectedOrderService = TestBed.inject(OrderService);
-  }));
-  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OrderComponent, MenuItemsComponent],
+      imports: [ReactiveFormsModule, RouterTestingModule],
+      providers: [
+        {
+          provide: RestaurantService,
+          useClass: MockRestaurantService,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: MockActivatedRoute,
+        },
+        {
+          provide: OrderService,
+          useClass: MockOrderService,
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+    orderService = TestBed.inject(OrderService);
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderComponent);
@@ -123,158 +126,175 @@ describe('OrderComponent', () => {
   });
 
   it('should get a restaurant based on route slug', () => {
-    let mockRestaurant = {
-      "name": "Poutine Palace",
-      "slug": "poutine-palace",
-      "images": {
-        "thumbnail": "node_modules/place-my-order-assets/images/4-thumbnail.jpg",
-        "owner": "node_modules/place-my-order-assets/images/3-owner.jpg",
-        "banner": "node_modules/place-my-order-assets/images/2-banner.jpg"
+    const mockRestaurant = {
+      name: 'Poutine Palace',
+      slug: 'poutine-palace',
+      images: {
+        thumbnail: 'node_modules/place-my-order-assets/images/4-thumbnail.jpg',
+        owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
+        banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
       },
-      "menu": {
-        "lunch": [
+      menu: {
+        lunch: [
           {
-            "name": "Crab Pancakes with Sorrel Syrup",
-            "price": 35.99
+            name: 'Crab Pancakes with Sorrel Syrup',
+            price: 35.99,
           },
           {
-            "name": "Steamed Mussels",
-            "price": 21.99
+            name: 'Steamed Mussels',
+            price: 21.99,
           },
           {
-            "name": "Spinach Fennel Watercress Ravioli",
-            "price": 35.99
-          }
+            name: 'Spinach Fennel Watercress Ravioli',
+            price: 35.99,
+          },
         ],
-        "dinner": [
+        dinner: [
           {
-            "name": "Gunthorp Chicken",
-            "price": 21.99
+            name: 'Gunthorp Chicken',
+            price: 21.99,
           },
           {
-            "name": "Herring in Lavender Dill Reduction",
-            "price": 45.99
+            name: 'Herring in Lavender Dill Reduction',
+            price: 45.99,
           },
           {
-            "name": "Chicken with Tomato Carrot Chutney Sauce",
-            "price": 45.99
-          }
-        ]
+            name: 'Chicken with Tomato Carrot Chutney Sauce',
+            price: 45.99,
+          },
+        ],
       },
-      "address": {
-        "street": "230 W Kinzie Street",
-        "city": "Green Bay",
-        "state": "WI",
-        "zip": "53205"
+      address: {
+        street: '230 W Kinzie Street',
+        city: 'Green Bay',
+        state: 'WI',
+        zip: '53205',
       },
-      "_id": "3ZOZyTY1LH26LnVw"
-    }
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    expect(fixture.componentInstance.restaurant).toEqual(mockRestaurant)
+      _id: '3ZOZyTY1LH26LnVw',
+    };
+    expect(fixture.componentInstance.restaurant).toEqual(mockRestaurant);
   });
 
   it('should have an orderForm formGroup', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    expect(fixture.componentInstance.orderForm.controls.restaurant).toBeTruthy();
-    expect(fixture.componentInstance.orderForm.controls.address).toBeTruthy();
-    expect(fixture.componentInstance.orderForm.controls.phone).toBeTruthy();
-    expect(fixture.componentInstance.orderForm.controls.items).toBeTruthy();
+    expect(
+      fixture.componentInstance.orderForm?.controls['restaurant']
+    ).toBeTruthy();
+    expect(
+      fixture.componentInstance.orderForm?.controls['address']
+    ).toBeTruthy();
+    expect(fixture.componentInstance.orderForm?.controls['phone']).toBeTruthy();
+    expect(fixture.componentInstance.orderForm?.controls['items']).toBeTruthy();
   });
 
   it('should have a validator on items formControl', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    let itemFormControl = fixture.componentInstance.orderForm.controls.items;
-    expect(itemFormControl.valid).toEqual(false);
+    const itemFormControl =
+      fixture.componentInstance.orderForm?.controls['items'];
+    expect(itemFormControl?.valid).toEqual(false);
   });
 
   it('should update items FormControl when setUpdatesItems is called', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    let childInput = compiled.getElementsByTagName('pmo-menu-items')[0].getElementsByTagName('input')[0];
-    let formItems = fixture.componentInstance.orderForm.get('items');
+    const compiled = fixture.nativeElement as HTMLElement;
+    const childInput = compiled
+      .getElementsByTagName('pmo-menu-items')[0]
+      .getElementsByTagName('input')[0];
+    const formItems = fixture.componentInstance.orderForm?.get('items');
     childInput.click();
     fixture.detectChanges();
-    expect(formItems.value).toEqual([{"name": "Crab Pancakes with Sorrel Syrup","price": 35.99}])
+    expect(formItems?.value).toEqual([
+      { name: 'Crab Pancakes with Sorrel Syrup', price: 35.99 },
+    ]);
   });
 
   it('should update the order total when the items FormControl value changes', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    let childInput1 = compiled.getElementsByTagName('pmo-menu-items')[0].getElementsByTagName('input')[0];
-    let childInput2 = compiled.getElementsByTagName('pmo-menu-items')[0].getElementsByTagName('input')[1];
+    const compiled = fixture.nativeElement as HTMLElement;
+    const childInput1 = compiled
+      .getElementsByTagName('pmo-menu-items')[0]
+      .getElementsByTagName('input')[0];
+    const childInput2 = compiled
+      .getElementsByTagName('pmo-menu-items')[0]
+      .getElementsByTagName('input')[1];
     childInput1.click();
     childInput2.click();
     fixture.detectChanges();
-    let orderText = compiled.querySelector('.submit h4');
-    expect(orderText.textContent).toEqual('Total: $57.98')
+    const orderText = compiled.querySelector('.submit h4');
+    expect(orderText?.textContent).toEqual('Total: $57.98');
   });
 
   it('should call the orderService createOrder on form submit with form values', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    let createOrderSpy = spyOn(injectedOrderService, 'createOrder').and.callThrough();
-    let expectedOrderValue = {
+    const createOrderSpy = spyOn(orderService, 'createOrder').and.callThrough();
+    const expectedOrderValue: OrderForm = {
       restaurant: '12345',
       name: 'Jennifer Hungry',
-      address:  '123 Main St',
-      phone: "555-555-5555",
-      items: [{name: "Onion fries", price: 15.99}, {name: "Roasted Salmon", price: 23.99}]
+      address: '123 Main St',
+      phone: '555-555-5555',
+      items: [
+        { name: 'Onion fries', price: 15.99 },
+        { name: 'Roasted Salmon', price: 23.99 },
+      ],
     };
-    let compiled = fixture.debugElement.nativeElement;
-    fixture.componentInstance.orderForm.setValue(expectedOrderValue);
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.orderForm?.setValue(expectedOrderValue);
     fixture.detectChanges();
-    compiled.querySelector('button[type="submit"]').click();
+    (
+      compiled.querySelector('button[type="submit"]') as HTMLButtonElement
+    ).click();
     expect(createOrderSpy).toHaveBeenCalledWith(expectedOrderValue);
   });
 
   it('should show completed order when order is complete', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    let expectedOrderValue = {
+    const expectedOrderValue: OrderForm = {
       restaurant: '12345',
       name: 'Jennifer Hungry',
-      address:  '123 Main St',
-      phone: "555-555-5555",
-      items: [{name: "Onion fries", price: 15.99}, {name: "Roasted Salmon", price: 23.99}]
+      address: '123 Main St',
+      phone: '555-555-5555',
+      items: [
+        { name: 'Onion fries', price: 15.99 },
+        { name: 'Roasted Salmon', price: 23.99 },
+      ],
     };
-    let compiled = fixture.debugElement.nativeElement;
-    fixture.componentInstance.orderForm.setValue(expectedOrderValue);
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.orderForm?.setValue(expectedOrderValue);
     fixture.detectChanges();
-    compiled.querySelector('button[type="submit"]').click();
+    (
+      compiled.querySelector('button[type="submit"]') as HTMLButtonElement
+    ).click();
     fixture.detectChanges();
-    let displayedOrder = compiled.querySelector('h3');
-    expect(displayedOrder.textContent).toEqual('Thanks for your order Jennifer Hungry!');
+    const displayedOrder = compiled.querySelector('h3');
+    expect(displayedOrder?.textContent).toEqual(
+      'Thanks for your order Jennifer Hungry!'
+    );
   });
 
   it('should clear the form values when create new order is clicked', () => {
-    const fixture = TestBed.createComponent(OrderComponent);
-    fixture.detectChanges();
-    let expectedOrderValue = {
+    const expectedOrderValue: OrderForm = {
       restaurant: '12345',
       name: 'Jennifer Hungry',
-      address:  '123 Main St',
-      phone: "555-555-5555",
-      items: [{name: "Onion fries", price: 15.99}, {name: "Roasted Salmon", price: 23.99}]
+      address: '123 Main St',
+      phone: '555-555-5555',
+      items: [
+        { name: 'Onion fries', price: 15.99 },
+        { name: 'Roasted Salmon', price: 23.99 },
+      ],
     };
-    let compiled = fixture.debugElement.nativeElement;
-    fixture.componentInstance.orderForm.setValue(expectedOrderValue);
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.componentInstance.orderForm?.setValue(expectedOrderValue);
     fixture.detectChanges();
-    compiled.querySelector('button[type="submit"]').click();
+    (
+      compiled.querySelector('button[type="submit"]') as HTMLButtonElement
+    ).click();
     fixture.detectChanges();
-    compiled.querySelector('button:nth-child(1)').click();
-    let emptyform = {
+    (
+      compiled.querySelector('button:nth-child(1)') as HTMLButtonElement
+    ).click();
+    const emptyform = {
       restaurant: '3ZOZyTY1LH26LnVw',
       name: null,
       address: null,
       phone: null,
-      items: []
-    }
-    expect(fixture.componentInstance.orderForm.value).toEqual(emptyform);
+      items: [],
+    };
+    expect(fixture.componentInstance.orderForm?.value).toEqual(emptyform);
   });
-
 });
