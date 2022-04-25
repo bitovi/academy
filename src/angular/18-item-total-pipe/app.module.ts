5d23f1382c5dbf13eb011cdd9afea7c672c887f0
fabioemoutinho <fabioemoutinho@gmail.com>
2022-04-25 15:31:49 -0300

    Page Updates: item-total-pipe - update files to angular 13

--- src/angular/18-item-total-pipe/app.module.ts
+++ src/angular/18-item-total-pipe/app.module.ts
@@ -1,13 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-
-import { MenuItemsComponent } from './order/menu-items/menu-items.component';
-import { HistoryComponent } from './order/history/history.component';
-import { ListComponent } from './order/list/list.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,31 +11,35 @@ import { RestaurantComponent } from './restaurant/restaurant.component';
 import { ImageUrlPipe } from './image-url.pipe';
 import { DetailComponent } from './restaurant/detail/detail.component';
 import { OrderComponent } from './order/order.component';
+import { MenuItemsComponent } from './order/menu-items/menu-items.component';
 import { OnlyNumbersDirective } from './only-numbers.directive';
+import { HistoryComponent } from './order/history/history.component';
+import { ListComponent } from './order/list/list.component';
 import { ItemTotalPipe } from './item-total.pipe';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        RestaurantComponent,
-        ImageUrlPipe,
-        DetailComponent,
-        OrderComponent,
-        MenuItemsComponent,
-        HistoryComponent,
-        ListComponent,
-        OnlyNumbersDirective,
-        ItemTotalPipe
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        TabsModule.forRoot()
-    ],
-    providers: [ItemTotalPipe],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RestaurantComponent,
+    ImageUrlPipe,
+    DetailComponent,
+    OrderComponent,
+    MenuItemsComponent,
+    OnlyNumbersDirective,
+    HistoryComponent,
+    ListComponent,
+    ItemTotalPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
+  ],
+  providers: [ItemTotalPipe],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/angular/18-item-total-pipe/item-total.pipe.ts
+++ src/angular/18-item-total-pipe/item-total.pipe.ts
@@ -1,11 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {Item} from "./order/order.service";
+import { Item } from './order/order.service';
 
 @Pipe({
-  name: 'itemTotal'
+  name: 'itemTotal',
 })
 export class ItemTotalPipe implements PipeTransform {
-
   transform(items: Item[]): number {
     let itemTotal = 0.0;
     if (items.length) {
@@ -16,5 +15,4 @@ export class ItemTotalPipe implements PipeTransform {
     }
     return itemTotal;
   }
-
 }
