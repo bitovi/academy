cbff0cf0c77000d422dbf1d338bff4af6fbc52fc
Christopher J Baker <christopher@bitovi.com>
2022-11-14 14:57:48 -0800

    cleanup workflow and dependencies

--- scripts/hubspot-publish/index.js
+++ scripts/hubspot-publish/index.js
@@ -1,12 +1,5 @@
 const HubSpotPublisher = require("./hubspot-publisher");
-const ghpages = require('gh-pages');
 
-ghpages.publish('academy', function(err) {
-	if(err) {
-		console.log("Publish failed with", err);
-	} else {
-    console.log("Success! Site uploaded to gh-pages")
-    const hubSpotPublisher = new HubSpotPublisher();
-    hubSpotPublisher.publish();
-  }
-});
+const hubSpotPublisher = new HubSpotPublisher();
+
+hubSpotPublisher.publish();
