87f5ac60007efd28d9dccc3eb93fbd84f2d304b9
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2023-03-03 16:10:08 -0500

    remove formatting changes

--- scripts/hubspot-deploy/hubspot-api.js
+++ scripts/hubspot-deploy/hubspot-api.js
@@ -1,15 +1,15 @@
-const axios = require("axios");
+const axios = require('axios');
 const Bottleneck = require("bottleneck");
 
 var rawStart = "{% raw %}",
-  rawEnd = "{% endraw %}";
+    rawEnd = "{% endraw %}";
 
 class HubSpotApi {
-  constructor(accessToken, campaignId) {
-    this.accessToken = accessToken;
+  constructor(apiKey, campaignId){
+    this.apiKey = apiKey;
     this.campaignId = campaignId;
-    this.baseUrl = "https://api.hubapi.com/content/api/v2/pages";
-    this.limiter = new Bottleneck({ minTime: 150 });
+    this.baseUrl = 'https://api.hubapi.com/content/api/v2/pages';
+    this.limiter = new Bottleneck({minTime: 150}),
     this.axios = axios.create({
       baseURL: this.baseUrl,
       headers: {
@@ -19,101 +19,89 @@ class HubSpotApi {
     });
   }
 
-  makeRequest(method, url, data) {
+  makeRequest(method, url, data){
     console.log("Making request:", method, url, data);
-    // return this.limiter.schedule(() =>
-    //   this.axios({ method, url, data }).catch((error) => {
+    // return this.limiter.schedule(() => this.axios({ method, url, data })
+    //   .catch(error => {
     //     if (data.name) {
     //       console.error(`Error on page ${data.name}`);
     //     }
 
-    //     console.error(
-    //       `Error ${error.response.status}: ${error.response.statusText}`
-    //     );
+    //     console.error(`Error ${error.response.status}: ${error.response.statusText}`);
     //     console.error(error.response.data);
 
     //     throw error;
-    //   })
-    // );
+    //   }));
   }
 
-  async getPages() {
+  async getPages(){
     const url = `&campaign=${this.campaignId}&limit=1000000`;
-    const response = await this.makeRequest("GET", url, {});
+    const response = await this.makeRequest('GET', url, {});
 
-    return response.data.objects
-      .map((page) => ({
-        campaign: page.campaign,
-        id: page.id,
-        slug: page.slug,
-      }))
-      .filter(
-        (page) =>
-          page.campaign === this.campaignId && page.slug.includes("academy")
-      );
+    return response.data.objects.map(page => ({
+      campaign: page.campaign,
+      id: page.id,
+      slug: page.slug
+    })).filter(page => page.campaign === this.campaignId && page.slug.includes('academy'));
   }
 
-  async createPage({ title, headHtml, bodyHtml, slug, metaDescription }) {
-    const url = ``;
+  async createPage( {title, headHtml, bodyHtml, slug, metaDescription } ){
     const data = {
       name: title,
-      template_path: "Custom/Page/Bitovi_July_2016_Theme/Academy.html",
+      template_path: 'Custom/Page/Bitovi_July_2016_Theme/Academy.html',
       slug: `${slug}`,
       html_title: title,
       is_draft: false,
       publish_immediately: true,
-      footer_html: rawStart + bodyHtml + rawEnd,
+      footer_html: rawStart+ bodyHtml + rawEnd,
       head_html: headHtml,
       campaign: this.campaignId,
-      subcategory: "site_page",
-      meta_description: metaDescription || "",
+      subcategory: 'site_page',
+      meta_description: metaDescription || ""
     };
-    const response = await this.makeRequest("POST", url, data);
+    const response = await this.makeRequest('POST', '', data)
     return this.publishPage(response.data.id);
   }
 
-  async updatePage(pageId, { title, headHtml, bodyHtml, metaDescription }) {
+  async updatePage(pageId, {title, headHtml, bodyHtml, metaDescription }){
     const url = `/${pageId}`;
     const data = {
       name: title,
       html_title: title,
-      footer_html: rawStart + bodyHtml + rawEnd,
+      footer_html: rawStart+ bodyHtml + rawEnd,
       head_html: headHtml,
-      meta_description: metaDescription || "",
-    };
+      meta_description: metaDescription || ""
+    }
     try {
-      const response = await this.makeRequest("PUT", url, data);
+      const response =  await this.makeRequest('PUT', url, data);
       console.log("Success! Updated page:", response.data.name);
       return response;
-    } catch (error) {
+    } catch(error) {
       console.error(error);
       throw error;
     }
   }
 
-  async publishPage(pageId) {
+  async publishPage(pageId){
     const url = `/${pageId}/publish-action`;
     const data = {
-      action: "schedule-publish",
-    };
-    const response = await this.makeRequest("POST", url, data);
-    console.log(
-      "Success! Created and published page:",
-      response.data.name || " no name - " + pageId
-    );
+      action: 'schedule-publish'
+    }
+    const response = await this.makeRequest('POST', url, data);
+    console.log("Success! Created and published page:", response.data.name || (" no name - "+pageId));
     return response;
   }
 
-  async deletePage(pageId) {
+  async deletePage(pageId){
     const url = `/${pageId}`;
-    const response = await this.makeRequest("DELETE", url, {});
+    const response = await this.makeRequest('DELETE', url, {})
     console.log("Success! Deleted page:", response.id);
     return response;
   }
 
-  async deleteAllPages() {
-    const pageIds = (await this.getPages()).map((page) => page.id);
-    pageIds.forEach((pageId) => this.deletePage(pageId));
+  async deleteAllPages(){
+    const pageIds = (await this.getPages()).map(page => page.id);
+    pageIds.forEach(pageId => this.deletePage(pageId));
   }
 }
 
