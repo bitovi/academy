3a04205f38bef2b1abf29e8f9df8685a543a6ff9
Christopher Baker <christopher@bitovi.com>
2023-03-07 11:13:30 -0800

    Update hubspot-api.js

--- scripts/hubspot-deploy/hubspot-api.js
+++ scripts/hubspot-deploy/hubspot-api.js
@@ -36,7 +36,7 @@ class HubSpotApi {
   }
 
   async getPages(){
-    const url = `&campaign=${this.campaignId}&limit=1000000`;
+    const url = `?campaign=${this.campaignId}&limit=1000000`;
     const response = await this.makeRequest('GET', url, {});
 
     return response.data.objects.map(page => ({
