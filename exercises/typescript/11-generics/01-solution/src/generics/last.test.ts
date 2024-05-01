065fe7fff6f2159e830eee2a32f2645439e96ef5
Chasen Le Hara <chasen@bitovi.com>
2024-04-30 18:45:47 -0700

    Improve the “Generics” page
    - Moved the exercise files to be inside a `generics` folder.
    - Renamed the test files to be one word so they’re easier to copy and paste.
    - Removed the `example.ts` files because they weren’t a part of the exercise.
    - Other minor edits.

--- exercises/typescript/11-generics/01-solution/src/generics/last.test.ts
+++ exercises/typescript/11-generics/01-solution/src/generics/last.test.ts
@@ -3,12 +3,12 @@ import { describe, it } from "node:test";
 import assert from 'node:assert/strict';
 
 describe("Generics", () => {
-  it("Returns last element with a string", () => {
+  it("returns last element with a string", () => {
     const lastString = returnLast<string>(["A", "B", "C"]);
 
     assert.equal(typeof lastString, "string", "It returns a string");
   });
-  it("Returns last element with a number", () => {
+  it("returns last element with a number", () => {
     const lastNumber = returnLast<number>([1, 2, 3]);
 
     assert.equal(typeof lastNumber, "number", "It returns a string");
