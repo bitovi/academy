c92fa3e19a3a0c78cfca11e3b9dbbf4c31a39535
DanDaManTran <DTran@bitovi.com>
2024-05-06 11:21:51 -0500

    03 test fix

--- exercise-src/react-native/09-design-system/03-solution/src/design/Button/Button.test.tsx
+++ exercise-src/react-native/09-design-system/03-solution/src/design/Button/Button.test.tsx
@@ -1,13 +1,21 @@
 import { fireEvent, render, screen } from "@testing-library/react-native"
 
+import ThemeProvider from "../theme/ThemeProvider"
+
 import Button from "./Button"
 
 describe("Button component", () => {
   it("renders title", () => {
     const handleChangeMock = jest.fn()
 
-    render(<Button onPress={handleChangeMock}>Hello!</Button>)
-
+    render(
+      <ThemeProvider>
+        <Button onPress={handleChangeMock}>
+          Hello!
+        </Button>
+      </ThemeProvider>
+    )
+    
     expect(screen.getByText(/Hello/)).toBeOnTheScreen()
     fireEvent.press(screen.getByText(/Hello/i))
     expect(handleChangeMock).toHaveBeenCalled()
--- exercise-src/react-native/09-design-system/03-solution/src/design/Card/Card.test.tsx
+++ exercise-src/react-native/09-design-system/03-solution/src/design/Card/Card.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react-native"
 
+import ThemeProvider from "../theme/ThemeProvider"
 import Typography from "../Typography"
 
 import Card from "./Card"
@@ -7,9 +8,11 @@ import Card from "./Card"
 describe("Card component", () => {
   it("renders title and children", () => {
     render(
-      <Card title="Hello!">
-        <Typography variant="body">How are you?</Typography>
-      </Card>,
+      <ThemeProvider>
+        <Card title="Hello!">
+          <Typography variant="body">How are you?</Typography>
+        </Card>
+      </ThemeProvider>
     )
 
     expect(screen.getByText(/Hello/)).toBeOnTheScreen()
