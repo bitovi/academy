94b5337a2f8fca4490ea8ddff5d77c3e397ec9de
Chasen Le Hara <chasen@bitovi.com>
2024-05-07 01:51:58 -0700

    Refine the “Creating a New Application” page
    - Fix the exercise code.
    - Pin React Native to `0.74.1`.
    - Add the Overview at the top of the page.
    - Add the exercise screenshot to the top of the Objective.
    - Move the image files.

--- exercise-src/react-native/03-creating-a-new-app/02-solution/App.tsx
+++ exercise-src/react-native/03-creating-a-new-app/02-solution/App.tsx
@@ -1,7 +1,7 @@
-import type React from 'react';
+import type { JSX } from "react"
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 
-function App(): React.JSX.Element {
+function App(): JSX.Element {
   return (
     <SafeAreaView style={{ height: "100%", width: "100%" }}>
       <ScrollView>
