66b171b677f189cc93d03e6f2b4926eb2e2896a9
DanDaManTran <Dtran@bitovi.com>
2024-05-08 16:52:03 -0500

    write the content for managing state with use state (#709)
    * context switching
    
    * Updated content for managing state

---
+++ src/react-native/10-managing-state/Toggle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Switch } from 'react-native';
+
+import { useToggle } from "./useToggle"
+
+const Toggle: React.FC = () => {
+  const [active, toggleActive] = useToggle(true);
+
+  return (
+    <View>
+      <View>
+        <Switch
+          onValueChange={toggleActive}
+          value={active}
+        />
+        <Text>{active ? "On" : "Off"}</Text>
+      </View>
+    </View>
+  );
+}
