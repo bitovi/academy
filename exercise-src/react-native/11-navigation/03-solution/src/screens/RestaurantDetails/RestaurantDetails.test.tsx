ca0ca155b6cde5837dea613e4f6788c96bd3c511
Chasen Le Hara <chasen@bitovi.com>
2024-05-08 11:02:59 -0700

    Fix the exercise 3 solution tests for Navigation
    The tests had all of the finished code in the app and needed to be pared down to what was needed just for this exercise.

--- exercise-src/react-native/11-navigation/03-solution/src/screens/RestaurantDetails/RestaurantDetails.test.tsx
+++ exercise-src/react-native/11-navigation/03-solution/src/screens/RestaurantDetails/RestaurantDetails.test.tsx
@@ -1,19 +1,8 @@
 import { render, screen } from "@testing-library/react-native"
 
-import * as restaurantHooks from "../../services/pmo/restaurant/hooks"
-import AuthProvider from "../../services/auth"
-
 import RestaurantDetails from "./RestaurantDetails"
 
 const params = {
-  state: {
-    name: "name",
-    short: "short",
-  },
-  city: {
-    name: "name",
-    state: "state",
-  },
   slug: "test",
 } as const
 
@@ -32,88 +21,9 @@ jest.mock("@react-native-async-storage/async-storage", () =>
 )
 
 describe("RestaurantDetails component", () => {
-  // Mock the hooks and components used in RestaurantDetails
-
-  let useRestaurant: jest.SpyInstance<
-    ReturnType<typeof restaurantHooks.useRestaurant>
-  >
-  beforeEach(() => {
-    jest.resetAllMocks()
-    useRestaurant = jest.spyOn(restaurantHooks, "useRestaurant")
-  })
-
-  const mockRestaurantData = {
-    data: {
-      _id: "1",
-      name: "Test Restaurant",
-      slug: "test-restaurant",
-      images: {
-        banner: "banner.jpg",
-        owner: "owner.jpg",
-        thumbnail: "thumbnail.jpg",
-      },
-      menu: {
-        dinner: [{ name: "yum", price: 1 }],
-        lunch: [{ name: "snack", price: 2 }],
-      },
-      coordinate: { latitude: 0, longitude: 0 },
-    },
-    isPending: false,
-    error: null,
-  }
-  it("renders loading state", () => {
-    useRestaurant.mockReturnValue({ data: null, isPending: true, error: null })
-    render(
-      <AuthProvider>
-        {/* @ts-ignore */}
-        <RestaurantDetails route={{ params }} />
-      </AuthProvider>,
-    )
-
-    expect(screen.getByText(/Loading/i)).toBeOnTheScreen()
-  })
-
-  it("renders error state", () => {
-    useRestaurant.mockReturnValue({
-      data: null,
-      isPending: false,
-      error: { name: "Error", message: "Mock error" },
-    })
-    render(
-      <AuthProvider>
-        {/* @ts-ignore */}
-        <RestaurantDetails route={{ params }} />
-      </AuthProvider>,
-    )
-    expect(
-      screen.getByText(/Error loading restaurant details:/i, {
-        exact: false,
-      }),
-    ).toBeOnTheScreen()
-    expect(screen.getByText(/Mock error/i)).toBeOnTheScreen()
-  })
-
   it("renders the RestaurantHeader and content when data is available", () => {
-    useRestaurant.mockReturnValue(mockRestaurantData)
-    render(
-      <AuthProvider>
-        {/* @ts-ignore */}
-        <RestaurantDetails route={{ params }} />
-      </AuthProvider>,
-    )
+    render(<RestaurantDetails route={{ params }} />)
 
-    expect(screen.getByText("Test Restaurant")).toBeOnTheScreen()
-  })
-
-  it("renders the RestaurantHeader and content when data is not available", () => {
-    useRestaurant.mockReturnValue({ ...mockRestaurantData, data: null })
-    render(
-      <AuthProvider>
-        {/* @ts-ignore */}
-        <RestaurantDetails route={{ params }} />
-      </AuthProvider>,
-    )
-
-    expect(screen.getByText("Place an order")).toBeOnTheScreen()
+    expect(screen.getByText("Cheese Curd City")).toBeOnTheScreen()
   })
 })
