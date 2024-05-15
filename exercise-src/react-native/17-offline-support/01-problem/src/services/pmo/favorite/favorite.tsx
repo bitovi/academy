bded03821f16464302f66c498913391e9feabddb
DanDaManTran <Dtran@bitovi.com>
2024-05-14 22:09:34 -0500

    Break down the objectives & exercises for “Adding Offline Support” (#737)
    Part of https://bitovi.atlassian.net/browse/LD-270
    
    Co-authored-by: Chasen Le Hara <chasen@bitovi.com>

---
+++ exercise-src/react-native/17-offline-support/01-problem/src/services/pmo/favorite/favorite.tsx
@@ -0,0 +1,10 @@
+import { useNetInfo } from "@react-native-community/netinfo"
+import { FC } from "react"
+
+import Typography from "../../../design/Typography"
+
+const FavoritesSync: FC = () => {
+  return <></>
+}
+
+export default FavoritesSync
---
+++ exercise-src/react-native/17-offline-support/01-solution/src/screens/Settings/Settings.tsx
@@ -0,0 +1,44 @@
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin"
+import { FC } from "react"
+import { Switch } from "react-native"
+
+import Button from "../../design/Button"
+import Card from "../../design/Card"
+import Screen from "../../design/Screen"
+import { useThemeMode } from "../../design/theme"
+import Typography from "../../design/Typography"
+import { useAuthentication, useUser } from "../../services/auth"
+import FavoritesSync from "../../services/pmo/favorite"
+
+const Settings: FC = () => {
+  const { signIn, signOut } = useAuthentication()
+  const user = useUser()
+  const { mode, setMode } = useThemeMode()
+
+  return (
+    <Screen>
+      <Card>
+        {user ? (
+          <>
+            <Typography variant="heading">Welcome back, {user.name}</Typography>
+            <Button onPress={signOut}>Sign Out</Button>
+          </>
+        ) : (
+          <GoogleSigninButton onPress={signIn} style={{ width: "100%" }} />
+        )}
+      </Card>
+      <Card>
+        <Typography variant="heading">Dark Mode</Typography>
+        <Switch
+          value={mode === "dark"}
+          onChange={() => setMode(mode === "light" ? "dark" : "light")}
+        />
+      </Card>
+      <Card>
+        <FavoritesSync />
+      </Card>
+    </Screen>
+  )
+}
+
+export default Settings
