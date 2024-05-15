b1220a27e5b07f8afab8ea53d32bd52a7a291a6b
Chasen Le Hara <chasen@bitovi.com>
2024-05-14 18:45:07 -0700

    Break down the objectives & exercises for “Security: Authentication and Authorization”
    Part of https://bitovi.atlassian.net/browse/LD-269

---
+++ exercise-src/react-native/16-security/01-problem/src/env.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly PMO_API: string
+      readonly PMO_ASSETS: string
+      readonly GOOGLE_OAUTH_CLIENT_ID: string
+    }
+  }
+}
+
+export {}
---
+++ exercise-src/react-native/16-security/01-problem/src/services/auth/AuthProvider.tsx
@@ -0,0 +1,64 @@
+import {
+  User as UserInfo,
+  GoogleSignin,
+} from "@react-native-google-signin/google-signin"
+import { FC, ReactNode, useCallback, useEffect, useMemo, useState } from "react"
+
+import { AuthContext, AuthContextProvider, useAuthContext } from "./context"
+
+const googleOauthwebClientId = process.env.GOOGLE_OAUTH_CLIENT_ID
+
+GoogleSignin.configure({
+  scopes: ["openid", "profile", "email"],
+  webClientId: googleOauthwebClientId,
+})
+
+const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>()
+
+  // Exercise: Finish implementing the `signIn` and `signOut` callbacks.
+  const signIn = useCallback(async () => {}, [])
+
+  const signOut = useCallback(async () => {}, [])
+
+  useEffect(() => {
+    // Exercise: When a sign in is successful, update the user.
+    async function run() {}
+
+    run()
+  }, [])
+
+  const value = useMemo<AuthContext>(
+    () => ({
+      signIn,
+      signOut,
+      isAuthenticated: userInfo ? true : userInfo === null ? false : undefined,
+      user: userInfo?.user,
+      scopes: userInfo?.scopes,
+      idToken: userInfo?.idToken,
+    }),
+    [signIn, signOut, userInfo],
+  )
+
+  return <AuthContextProvider value={value}>{children}</AuthContextProvider>
+}
+
+export default AuthProvider
+
+export function useAuthentication(): Pick<AuthContext, "signIn" | "signOut"> {
+  const { signIn, signOut } = useAuthContext()
+
+  return { signIn, signOut }
+}
+
+export function useAuthenticated(): boolean | undefined {
+  const { isAuthenticated } = useAuthContext()
+
+  return isAuthenticated
+}
+
+export function useUser(): UserInfo["user"] | undefined {
+  const { user } = useAuthContext()
+
+  return user
+}
