060f2ad0a204861c57c91a135c63e256b05c9e50
Attoh, Stefan <snoattoh@gmail.com>
2024-05-22 10:22:02 -0600

    prettier

--- exercise-src/react-native/09-react-context/03-problem/src/design/Button/Button.test.tsx
+++ exercise-src/react-native/09-react-context/03-problem/src/design/Button/Button.test.tsx
@@ -5,7 +5,7 @@ import Button from "./Button"
 describe("Design/Button", () => {
   it("renders", async () => {
     jest.useFakeTimers()
-    
+
     const user = userEvent.setup()
     const handleChangeMock = jest.fn()
 
@@ -13,13 +13,11 @@ describe("Design/Button", () => {
     const helloButton = screen.getByText(/Hello/)
 
     expect(helloButton).toBeOnTheScreen()
-    
 
     await user.press(helloButton)
     jest.advanceTimersByTime(10000)
     expect(handleChangeMock).toHaveBeenCalled()
-    
-    jest.useRealTimers()
 
+    jest.useRealTimers()
   })
 })
