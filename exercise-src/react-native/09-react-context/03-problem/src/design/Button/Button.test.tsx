54598656e5b0457d823a4b5eb2eb2693f2261d2a
Attoh, Stefan <snoattoh@gmail.com>
2024-05-22 10:08:35 -0600

    timers for Button

--- exercise-src/react-native/09-react-context/03-problem/src/design/Button/Button.test.tsx
+++ exercise-src/react-native/09-react-context/03-problem/src/design/Button/Button.test.tsx
@@ -2,20 +2,24 @@ import { userEvent, render, screen } from "@testing-library/react-native"
 
 import Button from "./Button"
 
-beforeEach(() => {
-  jest.resetAllMocks()
-  jest.useRealTimers()
-})
 describe("Design/Button", () => {
   it("renders", async () => {
+    jest.useFakeTimers()
+    
     const user = userEvent.setup()
     const handleChangeMock = jest.fn()
 
-    render(<Button onPress={handleChangeMock}>Hello!</Button>)
+    render(<Button onPress={handleChangeMock}>Hello</Button>)
     const helloButton = screen.getByText(/Hello/)
 
     expect(helloButton).toBeOnTheScreen()
+    
+
     await user.press(helloButton)
+    jest.advanceTimersByTime(10000)
     expect(handleChangeMock).toHaveBeenCalled()
+    
+    jest.useRealTimers()
+
   })
 })
