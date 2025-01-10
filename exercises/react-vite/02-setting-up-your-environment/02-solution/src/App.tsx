506d4316537ed36aa3437651fb45116ede8bf266
David Brandon <dbrandon@bitovi.com>
2025-01-10 16:16:48 -0600

    small react fixes

--- exercises/react-vite/02-setting-up-your-environment/02-solution/src/App.tsx
+++ exercises/react-vite/02-setting-up-your-environment/02-solution/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "./assets/vite.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
