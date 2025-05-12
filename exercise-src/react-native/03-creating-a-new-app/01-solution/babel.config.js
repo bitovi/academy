8047a4a0fa03342834b47d708fdb47f559e63794
DanDaManTran <DTran@bitovi.com>
2025-05-12 15:39:08 -0500

    conifg plugins requried extra set of brackets for tests

--- exercise-src/react-native/03-creating-a-new-app/01-solution/babel.config.js
+++ exercise-src/react-native/03-creating-a-new-app/01-solution/babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
+  plugins: [[
     "module-resolver", {
       root: ['./'],
       extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
       alias: {
         "@shared": ["./src/shared"]
       }
-  }],
+  }]]
 };
--- exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
+++ exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
   plugins: [
-    "module-resolver",
-    {
-      root: ["./"],
-      extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
-      alias: {
-        "@shared": ["./src/shared"],
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
+        alias: {
+          "@shared": ["./src/shared"],
+        },
       },
-    },
+    ],
   ],
 }
