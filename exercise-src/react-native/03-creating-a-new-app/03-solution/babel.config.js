47b99341e2c786cb89acd908d5e935d9b186e851
DanDaManTran <DTran@bitovi.com>
2025-05-12 14:47:14 -0500

    prettier

--- exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
+++ exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
   plugins: [
-    "module-resolver", {
-      root: ['./'],
+    "module-resolver",
+    {
+      root: ["./"],
       extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
       alias: {
-        "@shared": ["./src/shared"]
-      }
-  }],
-};
+        "@shared": ["./src/shared"],
+      },
+    },
+  ],
+}
