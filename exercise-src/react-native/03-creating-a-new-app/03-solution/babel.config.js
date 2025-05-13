0c9d3154da3ff12a0758c5348ec6a90a39be9bfe
DanDaManTran <Dtran@bitovi.com>
2025-05-13 11:48:29 -0500

    package and config update for alias (#918)
    * package and config update for alias
    
    * The config plugins required an extra set of brackets for tests

--- exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
+++ exercise-src/react-native/03-creating-a-new-app/03-solution/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
+        alias: {
+          "@shared": ["./src/shared"],
+        },
+      },
+    ],
+  ],
 }
--- exercise-src/react-native/13-http-requests/01-problem/babel.config.js
+++ exercise-src/react-native/13-http-requests/01-problem/babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  plugins: ["module:react-native-dotenv"],
   presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    "module:react-native-dotenv",
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".js", ".jsx", ".tsx", ".ts", ".ios.js", ".android.js"],
+        alias: {
+          "@shared": "./src/shared",
+        },
+      },
+    ],
+  ],
 }
