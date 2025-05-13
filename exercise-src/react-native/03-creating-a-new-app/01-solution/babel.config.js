4c8654caf43cf4e86ae4a33bddcf2a84a9ba370b
DanDaManTran <DTran@bitovi.com>
2025-05-13 10:13:46 -0500

    formating correction

--- exercise-src/react-native/03-creating-a-new-app/01-solution/babel.config.js
+++ exercise-src/react-native/03-creating-a-new-app/01-solution/babel.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [[
-    "module-resolver", {
-      root: ['./'],
-      extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
-      alias: {
-        "@shared": ["./src/shared"]
-      }
-  }]]
-};
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
+        alias: {
+          "@shared": ["./src/shared"],
+        },
+      },
+    ],
+  ],
+}
